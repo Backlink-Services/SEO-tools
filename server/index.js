0c0554d6bbc59896b181a550f22b210c2ed75390
Tricker14 <trietthetricker@gmail.com>
2024-07-30 10:59:46 +0700

    init backend

---
+++ server/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true // Allow credentials (cookies)
+  };
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '5mb'}));
+app.use(cookieParser());
+
+const dbURI = process.env.DB_URI;
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
