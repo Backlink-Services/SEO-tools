d3551c791ce27758dba79c6262c394384291e35c
Hoang Minh Thang <thangminhhoang7112002@gmail.com>
2024-08-06 10:04:04 +0700

    Chuc nang

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const router = express.Router();
-const postController = require("../controllers/postController");
+const express = require('express')
+const router = express.Router()
+const postController = require('../controllers/postController')
 
-router.post("/post", postController.createPost);
-router.put("/post/:id", postController.updatePost);
-router.delete("/post/:id", postController.deletePost);
+router.get('/post', postController.getAllPost)
+router.post('/post', postController.createPost)
+router.put('/post/:id', postController.updatePost)
+router.delete('/post/:id', postController.deletePost)
 
-router.post("/comment", postController.postComment);
+router.post('/comment', postController.postComment)
 
-module.exports = router;
+module.exports = router
